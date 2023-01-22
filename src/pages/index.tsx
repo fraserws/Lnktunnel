07df0759d39036bdfa6ef2371a5038ecfbbee2f2
fraserws <fraserws.uk2@gmail.com>
2023-01-22 21:18:35 +0000

    reading data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { supabase } from "../../utils/supabaseClient";
 import { useEffect, useState } from "react";
 
+type Link = {
+  id: string;
+  title: string;
+  url: string;
+  user_id: string;
+};
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [userId, setUserId] = useState<string | undefined>(undefined);
   const [title, setTitle] = useState<string | undefined>(undefined);
   const [url, setUrl] = useState<string | undefined>(undefined);
+  const [links, setLinks] = useState<Link[]>();
   useEffect(() => {
     const getUser = async () => {
       const user = await supabase.auth.getUser();
@@ -18,14 +25,40 @@ export default function Home() {
     };
     getUser();
   }, []);
+
+  useEffect(() => {
+    const getLinks = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("links")
+          .select("title, url")
+          .eq("user_id", userId);
+
+        if (error) throw error;
+        //@ts-ignore
+        setLinks(data);
+        console.log("links:", links);
+        console.log("data:", data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    if (userId) {
+      getLinks();
+    }
+  }, [userId]);
+
   const addNewLink = async () => {
     try {
       if (title && url && userId) {
-        const { data, error } = await supabase.from("links").insert({
-          title: title,
-          url: url,
-          user_id: userId,
-        });
+        const { data, error } = await supabase
+          .from("links")
+          .insert({
+            title: title,
+            url: url,
+            user_id: userId,
+          })
+          .select();
         if (error) throw error;
         console.log("data:", data);
       }
