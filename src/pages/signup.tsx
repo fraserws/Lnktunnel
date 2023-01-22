249a71574d2897af3ba371816e6e4f1b3423dbf1
fraserws <fraserws.uk2@gmail.com>
2023-01-22 19:14:38 +0000

    basic auth

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,16 +1,14 @@
 import * as Z from "zod";
-import { useRouter } from "next/router";
 import { useState } from "react";
 import { supabase } from "../../utils/supabaseClient";
 
 const emailSchema = Z.string().email();
 
-export default function Login() {
+export default function Signup() {
   const [email, setEmail] = useState<string | undefined>();
   const [password, setPassword] = useState<string | undefined>();
-  const router = useRouter();
 
-  async function signInWithEmail() {
+  async function signUpWithEmail() {
     try {
       emailSchema.parse(email);
     } catch {
@@ -19,14 +17,10 @@ export default function Login() {
     }
     try {
       if (email && password) {
-        const resp = await supabase.auth.signInWithPassword({
-          email,
-          password,
-        });
+        const resp = await supabase.auth.signUp({ email, password });
         if (resp.error) throw resp.error;
         const userId = resp.data.user?.id;
         console.log("userId", userId);
-        router.push("/");
       }
     } catch {}
   }
@@ -62,7 +56,7 @@ export default function Login() {
       </div>
       <button
         className="from-indigo-primary btn w-full max-w-xs bg-gradient-to-r from-primary to-secondary"
-        onClick={signInWithEmail}
+        onClick={signUpWithEmail}
       >
         Login
       </button>
